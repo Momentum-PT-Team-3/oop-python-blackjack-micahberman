a2730bc761d5358b9c8781fd5f4efe9b27a1013a
micahberman <micahdberman@gmail.com>
2021-12-15 16:49:35 -0500

    finally gets dealer to loop

--- blackjack.py
+++ blackjack.py
@@ -5,16 +5,34 @@ SUITS = ["Spades", "Clubs", "Diamonds", "Hearts"]
 SCORES = {2 : 2, 3 : 3, 4 : 4, 5 : 5, 6 : 6, 7 : 7, 8 : 8, 9 : 9, 10 : 10, "J" : 10, "Q" : 10, "K" : 10, "A" : 11}
 
 class Card:
-    def __init__(self, suit, rank, score):
+    def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
-        self.score = score
+        self.value = self.calculate_value(self.rank)
         
     def __str__(self):
-        return f'{self.rank} of {self.suit} {self.score}'
+        return f'{self.rank} of {self.suit}'
 
-    def get_score(self):
-        return f'{self.score}'
+    def calculate_value(self, rank):
+        values_dictionary = {
+            2:2,
+            3:3,
+            4:4,
+            5:5,
+            6:6,
+            7:7,
+            8:8,
+            9:9,
+            10:10,
+            "J":10,
+            "Q":10,
+            "K":10,
+            "A":11
+        }
+        card_value = values_dictionary[rank]
+        return card_value
+
+    
 
 
 class Deck:
@@ -23,7 +41,7 @@ class Deck:
    
         for suit in suits:
             for value in scores.keys():
-                card = Card(suit, value, scores.get(value))
+                card = Card(suit, value)
                 self.cards.append(card)
                 # cardscore = card.score
                 # print (cardscore)
@@ -71,6 +89,8 @@ class Game:
         self.player = Player()
         self.dealer = Dealer()
         self.gamedeck = Deck(suits, scores)
+        self.player_score = 0
+        self.dealer_score = 0
 
     def deal_cards(self):
         for i in range(2):
@@ -79,16 +99,25 @@ class Game:
         self.dealer.show_hand()
         self.player.show_hand()
 
-    def deal_dealer(self, total):
-        while total <= 17:
-            self.hit(self.dealer)
-        else:
-            self.dealer.show_hand()
 
     def hit(self, person):
-        
         person.hand.append(self.gamedeck.draw_card())
         person.show_hand()
+
+    def calculate_totals(self, dealer):
+        self.dealer_hand_values = []
+        for card in dealer.hand:
+            self.dealer_hand_values.append(card.value)
+        self.dealer_score = sum(self.dealer_hand_values)
+    
+    def deal_dealer(self):
+        while self.dealer_score < 17:
+            self.hit(self.dealer)
+            self.calculate_totals(self.dealer)
+            print(self.dealer_score)
+        else:
+            print('Hit 17, stopping.')
+            print(self.dealer_score)
 
     def hit_or_stay(self):
         choice = input("Would you like to hit? y/n")
@@ -102,7 +131,8 @@ class Game:
 
 game = Game(SUITS, SCORES)
 game.deal_cards()
-game.hit_or_stay()
+# game.hit_or_stay()
+game.deal_dealer()
 
 
 
